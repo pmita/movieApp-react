4cb2f5ab5836fe11bf18adab74dc09cfc32044bc
Panos Mita <panos_mita@epam.com>
2021-09-20 15:15:35 +0100

    Husky, ESLint, Lint-staged, mini-css-extract-plugin added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
 import ReactDOM from 'react-dom';
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path = require('path');
+import { join } from 'path';
+// eslint-disable-next-line no-undef
+import MiniCssExtractPlugin, { loader as _loader } from 'mini-css-extract-plugin';
 
-module.exports = {
-    output : {
-        path : path.join(__dirname, '/dist'),
-        filename : 'index.bundle.js',
-    },
-    devServer : {
-        historyApiFallback : true, 
-        port : 4000,
-        open : true,
-        hot : true
-    },
-    module : {
-        rules : [
-            {
-                test : /\.(js|jsx)$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : 'babel-loader'
-                }
-            },
-            {
-                test : /\.scss$/,
-                use : [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
+export const output = {
+    // eslint-disable-next-line no-undef
+    path     : join(__dirname, '/dist'),
+    filename : 'index.bundle.js'
+};
+export const devServer = {
+    historyApiFallback : true,
+    port               : 4000,
+    open               : true,
+    hot                : true
+};
+export const module = {
+    rules : [
+        {
+            test    : /\.(js|jsx)$/,
+            exclude : /node_modules/,
+            use     : {
+                use : [ 'babel-loader', 'eslint-loader' ]
             }
-        ]
-    }
-}
+        },
+        {
+            test : /\.scss$/,
+            use  : [
+                _loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        }
+    ]
+};
+export const plugins = [ new MiniCssExtractPlugin() ];
