99b3df8ca7adb3f20735b0949b2827084913e173
pmita <panos.mita2@outlook.com>
2021-09-29 10:09:33 +0100

    Feat/Migrating all state to Context api

--- src/store/ProjectContext.js
+++ src/store/ProjectContext.js
@@ -1,4 +1,7 @@
-import React, {useState, createContext} from 'react';
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect, createContext} from 'react';
+// import our assets
+import {mockData} from '../assets/data/MockData';
 
 export const ProjectContext = createContext();
 
@@ -6,9 +9,40 @@ export const ProjectProvider = (props) => {
     // defining our state
     // const [testValue, setTestValue] = useState('Test from Test Value');
     const [isHidden, setIsHidden] = useState(true);
+    const [movieItem, setMovieItem] = useState({});
+    const [movies, setMovies] = useState([]);
+    const [moviesToShow, setMoviesToShow] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [filter, setFilter] = useState('');
+
+    // define our useEffect actions
+    useEffect(() => {
+        setMovieItem({
+            name : '', date : '', category : '', rating : '', img : '', overview : '', id : ''
+        });
+        setMovies(mockData);
+        setMoviesToShow(mockData);
+        setCategories([
+            { name: 'ALL', active: true },
+            { name: 'DOCUMENTARY', active: false },
+            { name: 'COMEDY', active: false },
+            { name: 'HORROR', active: false },
+            { name: 'ACTION', active: false }
+        ]);
+        setFilter('RELEASE DATE');
+    }, [])
 
     return(
-	<ProjectContext.Provider value={[ isHidden, setIsHidden ]}>
+	<ProjectContext.Provider 
+		value={[ 
+            isHidden, setIsHidden, 
+            movieItem, setMovieItem,
+            movies, setMovies,
+            moviesToShow, setMoviesToShow,
+            categories, setCategories,
+            filter, setFilter
+        ]}
+	>
 		{props.children}
 	</ProjectContext.Provider>
     )
