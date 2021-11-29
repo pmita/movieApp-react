20ab730af2bf51ec9d931d3bb6505b5bf9b73323
pmita <panos.mita2@outlook.com>
2021-11-29 09:50:55 +0000

    Feature/Unnecessary items removed

--- src/hooks/useReduxState.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-export const useReduxState = async () => {
-    // REDUX STATE
-    const reduxState = await useSelector((state) => state.movieApp);
-
-    return reduxState ;
-}
