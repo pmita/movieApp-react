9a0535b75e4f0d2deed48fa3ea8df839e62c9ea9
pmita <panos.mita2@outlook.com>
2021-11-29 15:14:25 +0000

    Feature/ModieDetail component re-factoring

--- src/features/Banner/component.js
+++ src/features/Banner/component.js
@@ -1,16 +1,11 @@
 /* eslint-disable max-len */
 import React from 'react';
 import style from './style.module.scss';
-import { useSelector } from 'react-redux'; // redux state
 // import components
 import Navbar from '../../shared/Navbar';
 import SearchMovie from '../../shared/SearchMovie';
-import MovieDetails from '../MovieDetails/component';
 
 const Banner = () => {
-	// REDUX STATE
-	const showMovie = useSelector((state) => state.movieApp.showMovie);
-
 	return(
 		<section className={style.bannerSection}>
 			<Navbar />
@@ -19,12 +14,8 @@ const Banner = () => {
 				src='https://images.pexels.com/photos/7991565/pexels-photo-7991565.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260' 
 				alt='background for our banner'
 			/>
-			{showMovie
-				? <MovieDetails 
-						showMovie={showMovie}  
-				  />
-				: <SearchMovie />
-			}
+
+			<SearchMovie />
 		</section>
 	);
 } 
--- src/features/MovieItem/component.js
+++ src/features/MovieItem/component.js
@@ -5,8 +5,6 @@ import './style.scss';
 import { useDispatch } from 'react-redux';
 import { removeMovie } from '../../store/actions/actionCreators';
 // COMPONENTS
-// import EditMovie from '../../shared/EditMovie';
-import MovieDetails from '../MovieDetails/component';
 import { array } from 'prop-types';
 import AddMovie from '../../shared/AddMovie/component';
 // ROUTER
@@ -15,21 +13,22 @@ import { Link } from 'react-router-dom'
 const MovieItem = ({ movie, id }) => {
 	// REDUX & STATE
 	const [isHidden, setIsHidden] = useState(true);
-	const [toggleMovieDetails, setToggleMovieDetails] = useState(false);
 	const dispatch = useDispatch();
 
 	// EVENT HANDLERS
-	// const showMovieDetailsHandler = useCallback(() => dispatch(showMovieDetails(id)), [dispatch, showMovieDetails, id]);
 	const removeMovieHandler = useCallback(() => dispatch(removeMovie(id)), [dispatch, removeMovie, id]);
 	const editMovieDetailsHandler = useCallback(() => setIsHidden(!isHidden), [isHidden, setIsHidden]);
-	const showMovieDetailsHanlderVol2 = () => setToggleMovieDetails(!toggleMovieDetails);
+
 	return(
 		<>
 			<div className='movieItem-section'>
-				<img src={movie.poster_path} alt='random text' onClick={showMovieDetailsHanlderVol2}/>
-				<button>
-					<Link to={`/films/${id}`}>Check movie</Link>
-				</button>
+				<Link to={`/films/${id}`}>
+					<img 
+						src={movie.poster_path} 
+						alt='random text' 
+					/>
+				</Link>
+
 				<ul className='movieItem-details'>
 					<h4>{movie.title}</h4>
 					<h4>{movie.release_date}</h4>
@@ -41,23 +40,26 @@ const MovieItem = ({ movie, id }) => {
 				}
 				
 				<div className='movieItem-buttons'>
-					<button className='btn btn-editItem' onClick={editMovieDetailsHandler}>Edit</button>
-					<button className='btn btn-removeItem' onClick={removeMovieHandler} >Remove</button>
+					<button 
+						className='btn btn-editItem' 
+						onClick={editMovieDetailsHandler}
+					>
+						Edit
+					</button>
+					<button 
+						className='btn btn-removeItem' 
+						onClick={removeMovieHandler} 
+					>
+						Remove
+					</button>
 				</div>
 			</div>
+
 			{!isHidden &&
 				<AddMovie
 					movie={movie}
 					toggleAddMovie={isHidden}
 					setToggleAddMovie={setIsHidden}
-				/>
-			}
-			{toggleMovieDetails &&
-				<MovieDetails 
-					movie={movie}
-					id={id}
-					toggleMovieDetails={toggleMovieDetails}
-					setToggleMovieDetails={setToggleMovieDetails}
 				/>
 			}
 		</>
